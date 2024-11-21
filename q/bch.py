30a43c6dec51b4970954f48d103ee82c11f27dde
vv4t <watashidesu6969@gmail.com>
2024-11-22 01:44:26 +1100

    single error thing

--- q/bch.py
+++ q/bch.py
@@ -4,42 +4,54 @@ m1 = Polynomial([1,0,0,1,1], Z(2))
 m3 = Polynomial([1,1,1,1,1], Z(2))
 m = m1 * m3
 
-message = "0010111"
+message = "1001011"
 message = [ int(s) for s in message ]
 
 C_I = Polynomial(list(reversed(message)) + [0] * 8, Z(2))
 C_R = C_I % m
 C_x = C_I + C_R
 
-print("encoded message", C_x, "".join(map(str, C_x.coeff)))
+print(C_x)
+print("".join(map(str, C_x.coeff)))
 
 print("----------------------------")
 a = Polynomial([1,0], Z(2))
-for i in range(15):
+for i in range(16):
   print(i, "->", a ** i % m1)
 print("----------------------------")
 
-d = "100010011111000"
+d = "100101000011111"
 d = [ int(s) for s in d ]
 
 D_x = Polynomial(list(reversed(d)), Z(2))
 
 # here, match syndromes to primitive power
 
-print(D_x.evaluate(a) % m1)
-print(D_x.evaluate(a ** 3) % m1)
+D_a = D_x.evaluate(a) % m1
+D_a3 = D_x.evaluate(a ** 3) % m1
+
+print(D_a)
+print((D_a ** 3) % m1)
+print(D_a3)
+
+if D_a3 == (D_a**3) % m1:
+  print("single error")
+  d[13] = (d[13] + 1) % 2
+  y = "".join(map(str,d))
+  print(y[:8], y[8:])
+  exit()
 
 # then change what the i,j pair should look for
 
 pairs = []
 
-for i in range(15):
-  for j in range(i+1, 15):
-    if (a**i + a**j) % m1 == (a**12 % m1):
+for i in range(16):
+  for j in range(i+1, 16):
+    if (a**i + a**j) % m1 == (a**13 % m1):
       pairs.append((i, j))
 
 for x,y in pairs:
-  if (a**(3*x) + a**(3*y)) % m1 == (a**11 % m1):
+  if (a**(3*x) + a**(3*y)) % m1 == (a**9 % m1):
     print(x,y)
     break
 
