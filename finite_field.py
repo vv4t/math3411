a9a3fb6610c9401bf1cb196f10681a505f36639d
vv4t <watashidesu6969@gmail.com>
2024-11-21 23:21:01 +1100

    whatever

--- finite_field.py
+++ finite_field.py
@@ -7,7 +7,7 @@ def is_int(b):
   except:
     return False
 
-class GF_P:
+class Z_p:
   def __init__(self, P, N):
     self.P = int(P)
     self.N = int(N)
@@ -17,17 +17,17 @@ class GF_P:
     self.N = self.N % self.P
   
   def copy(self):
-    return GF_P(self.P, self.N)
+    return Z_p(self.P, self.N)
   
   def inverse(self):
     r, x, y = egcd(self.N, self.P)
-    return GF_P(self.P, x)
+    return Z_p(self.P, x)
   
   def __mod__(a, b):
-    return GF_P(self.P, a.N % int(b)) if is_int(b) else NotImplemented
+    return Z_p(self.P, a.N % int(b)) if is_int(b) else NotImplemented
   
   def __neg__(self):
-    return GF_P(self.P, -self.N)
+    return Z_p(self.P, -self.N)
   
   def __radd__(b, a):
     return b + a if is_int(a) else NotImplemented
@@ -36,19 +36,19 @@ class GF_P:
     return b * a if is_int(a) else NotImplemented
   
   def __add__(a, b):
-    return GF_P(a.P, int(a) + int(b)) if is_int(b) else NotImplemented
+    return Z_p(a.P, int(a) + int(b)) if is_int(b) else NotImplemented
   
   def __sub__(a, b):
-    return GF_P(a.P, int(a) - int(b)) if is_int(b) else NotImplemented
+    return Z_p(a.P, int(a) - int(b)) if is_int(b) else NotImplemented
   
   def __mul__(a, b):
-    return GF_P(a.P, int(a) * int(b)) if is_int(b) else NotImplemented
+    return Z_p(a.P, int(a) * int(b)) if is_int(b) else NotImplemented
   
   def __eq__(a, b):
     return (int(a) - int(b)) % a.P == 0 if is_int(b) else NotImplemented
   
   def __truediv__(a, b):
-    return a * GF_P(a.P, int(b)).inverse() if is_int(b) else NotImplemented
+    return a * Z_p(a.P, int(b)).inverse() if is_int(b) else NotImplemented
   
   def __int__(self):
     return self.N
@@ -56,5 +56,5 @@ class GF_P:
   def __repr__(self):
     return str(self.N)
 
-def GF(P):
-  return lambda n : GF_P(P, n)
+def Z(P):
+  return lambda n : Z_p(P, n)
--- math3411.py
+++ math3411.py
@@ -3,5 +3,5 @@ from information_theory import *
 from number_theory import *
 from compression_coding import *
 from polynomial import *
-from galois import *
+from finite_field import Z
 from error_code import *
