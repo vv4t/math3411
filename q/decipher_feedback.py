977cc46472c830c63153d7396aef78bdd8624f98
vv4t <watashidesu6969@gmail.com>
2024-11-22 22:31:03 +1100

    new file

---
+++ q/decipher_feedback.py
@@ -0,0 +1,18 @@
+from math3411 import *
+
+# modelled off 2014 q3 iii
+
+# by itself, it works like vigenere cipher rotating with MATH as key
+shifts = shifted_set_from_word("MATH")
+print(alphabetic_decrypt("QXTTTXSETD", shifts))
+
+# start of the ciphertext looks like EXAM so assume that as plaintext
+# we now try either ciphertext feedback or plaintext feedback
+
+shifts = shifted_set_from_word("MATHXTTTXSETD")
+print(alphabetic_decrypt("QXTTTXSETD", shifts))
+
+shifts = shifted_set_from_word("MATHEXAM")
+print(alphabetic_decrypt("QXTTTXSETD", shifts))
+
+# plaintext feedback decrypts to EXAMPASSHD
